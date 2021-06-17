20c1d200ade5fb9d984d430c10568b5bff6795a2
Dustin Martinez <martinezdustin736@gmail.com>
2021-06-17 16:33:30 -0500

    completed tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,13 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums(
 
-)
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+    artist VARCHAR (100) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    release_date INT NOT NULL,
+    sales DOUBLE NOT NULL,
+    genre VARCHAR (50) NOT NULL,
+PRIMARY KEY (id)
+);
