82021d9c03babfe260b37925fcce91308861f614
Dustin Martinez <martinezdustin736@gmail.com>
2021-06-18 11:59:50 -0500

    added select_exercises

---
+++ albums_seeder.sql
@@ -0,0 +1,29 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, genre, sales)
+values ('Various artist','Grease The Original Soundtrack from the Motion Picture',1978,'Rock and roll',14.4),
+       ('Led Zepplin', 'Led Zeppelin IV',1971, 'Hard rock, heavy metal, folk rock',29.0),
+       ('Michael Jackson', 'Bad', 1987,'Pop, rhythm and blues, funk and rock',22.2),
+       ('Alanis Morissette','Jagged Little Pill',1995,'Alternative rock',24.4),
+       ('Michael Jackson','Dangerous',1991,'New jack swing, R&B and pop',17.0),
+       ('Celine Dion','Falling into You',1996,'Pop, soft rock',17.9),
+       ('Eagles', 'Hotel California', 1976,'Soft Rock',31.5),
+       ('The Beatles','Sgt. Pepper''s Lonely Hearts Club Band',1967,'Rock',18.2),
+       ('Various artist', 'Dirty Dancing', 1987,'Pop, rock, R&B',17.9),
+       ('Adele','21',2011,'Pop,soul',30.8),
+       ('Madonna','The Immaculate Collection',1990,'Pop,dance',19.5),
+       ('Celine Dion','Let''s Talk About Love',1998,'Pop, soft rock',19.3),
+       ('Metallica','Metallica',1991,'Heavy metal',25.2),
+       ('The Beatles','1',2000,'Rock',23.4),
+       ('The Beatles','Abbey Road',1969,'Rock',23.4),
+       ('ABBA','Gold:Greatest Hits',1991,'Pop,disco',23.0),
+       ('Bruce Springsteen','Born in the U.S.A.',1984,'Heartland Rock',19.6),
+       ('Pink Floyd', 'The Wall',1979,'Progressive rock',18.1=7),
+       ('James Horner', 'Titanic: Music from the Motion Picture',1997,'Film score',18.1),
+       ('Dire Straits', 'Brothers in Arms',1985,'Roots rock, blues rock, soft rock',17.7),
+       ('Nirvana','Nevermind',1991,'Grunge, alternative rock',16.7),
+       ('Santana', 'Supernatural',1999,'Latin rock',20.5),
+       ('Guns N'' Roses','Appetite for Destruction',1987,'Hard rock',21.9),
+       ('Elton John','Goodbye Yellow Brick Road',1973,'rock, pop rock, glam rock',8.5);
+
+
---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+SELECT 'All from Pink Floyd' AS 'Albums';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Albums';
+SELECT release_date FROM albums WHERE name = 'Sgt.Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind' AS 'Nirvana album';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Albums released in the 90s' AS '90s albums';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Less than 20 mil certified sales' AS 'Certified Sales';
+SELECT * FROM albums WHERE sales < 20;
+
+SELECT 'Rock albums' AS 'Genre: Rock';
+SELECT * FROM albums WHERE genre = 'Rock';
